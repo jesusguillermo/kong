e22452f1b8291dc550bc9e0993700597ba7f5a4c
jesusguillermo <whitedemon_90@hotmail.com>
2014-07-15 21:57:28 -0500

    metodo update gato

--- Shoppu/src/com/me/objetos/Gato.java
+++ Shoppu/src/com/me/objetos/Gato.java
@@ -65,14 +65,11 @@ public class Gato {
 		if(state != State.muerto && vida != 0)
 		{
 			body.setLinearVelocity(velocidad.x,velocidad.y);
-			Gdx.app.log("VElcidad X", velocidad.x + "");
-			Gdx.app.log("VELOCIDAD Y", velocidad.y + "");
 			Gdx.app.log("bolean primer", primer + "");
 			Gdx.app.log("bolean segundo", segundo + "");
 			if(state == State.boos  )
 			{
 				primer = false;
-				Gdx.app.log("state == boos",  "");
 				timer+= delta;
 				if (timer >= .5f) 
 				{
@@ -82,7 +79,6 @@ public class Gato {
 				}
 				if(segundo)
 				{
-					Gdx.app.log("state == boos , segundo",  "");
 					body.setLinearVelocity(10, 5);
 				}
 				else
@@ -90,33 +86,28 @@ public class Gato {
 			}	
 				if (primer && state != State.fly) 
 			{
-				Gdx.app.log("primer",  "");
 				state = State.corriendo;
 				statetime = 0;
 				//para que sale mas		
 				body.setLinearVelocity(2, velocidad.y);
-				if(segundo )
+				
+			}						
+				if(segundo && state != State.saltando || segundo && state != State.cayendo)
 				{
-					Gdx.app.log("segundo",  "");
 				state = State.saltando;
 				statetime = 0;
 				body.setLinearVelocity(2, 5);
 				}
-			}						
-			
 			 if(state == State.saltando )
 			{
-				Gdx.app.log("state == State.staltando",  "");
 				if (velocidad.y < 0 && state != State.cayendo)
 				{
-					Gdx.app.log("velocidad.y < 0 && state != State.cayendo",  "");
 					state = State.cayendo;
 					statetime = 0;
 				}
 			}
 			 if(state == State.fly)
 			{				
-				Gdx.app.log("state == State.fly",  "");
 				if (velocidad.y > 3) {
 					velocidad.y = 3;
 					body.setLinearVelocity(velocidad);
@@ -157,7 +148,6 @@ public class Gato {
 	public void jump() {
 		if (state == State.cayendo || state == State.fly || state == State.corriendo) 
 		{				
-			Gdx.app.log("Metodo Jump()",  "");
 			state = State.standing;	
 			statetime = 0;
 		}
